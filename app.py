ccc80f92a2e71406accef9664c2e87786379a82f
Anda <at13170@my.bristol.ac.uk>
2017-02-12 17:47:58 +0000

    Changing combinations of font size and layout directly from backend :tada:

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file, jsonify
+import random as rand
+from itertools import product
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_file('templates/index.html')
+	return render_template('index.html', layout=generate_layout())
+
+def generate_layout():
+	layouts = ["grid", "list"]
+	font_sizes = ["small", "large"]
+	colour_schemes = ["dark", "light"]
+	versions = list(product(layouts, font_sizes, colour_schemes))
+	choice = versions[rand.randint(0, len(versions) - 1)]
+	choice = {'layout': choice[0], 'fontSize': choice[1], 'colourScheme': choice[2]}
+	return choice
 
 if __name__ == "__main__":
-    app.run(host="localhost", debug=True)
+	app.run(host="localhost", debug=True)
--- static/js/controllers/index.controller.js
+++ static/js/controllers/index.controller.js
@@ -1,32 +1,36 @@
-angular.module('app').controller('indexController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+angular.module('app').controller('indexController', ['$scope', '$rootScope', '$http', '$window', function ($scope, $rootScope, $http, $window) {
 
 	var ctrl = this;
+
+	ctrl.layout = $window.layoutType.layout;
+	ctrl.fontSize = $window.layoutType.fontSize;
 	
 	ctrl.posts = [
-	{
-		'title' : 'Blog post #1',
-		'thumbnail' : 'static/images/pic01.jpg',
-		'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
-	},
-	{
-		'title' : 'Blog post #2',
-		'thumbnail' : 'static/images/pic02.jpg',
-		'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
-	},
-	{
-		'title' : 'Blog post #3',
-		'thumbnail' : 'static/images/pic03.jpg',
-		'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
-	},
-	{
-		'title' : 'Blog post #4',
-		'thumbnail' : 'static/images/pic04.jpg',
-		'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
-	},
-	{
-		'title' : 'Blog post #5',
-		'thumbnail' : 'static/images/pic05.jpg',
-		'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
-	},
-	]
+		{
+			'title' : 'Blog post #1',
+			'thumbnail' : 'static/images/pic01.jpg',
+			'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+		},
+		{
+			'title' : 'Blog post #2',
+			'thumbnail' : 'static/images/pic02.jpg',
+			'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+		},
+		{
+			'title' : 'Blog post #3',
+			'thumbnail' : 'static/images/pic03.jpg',
+			'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+		},
+		{
+			'title' : 'Blog post #4',
+			'thumbnail' : 'static/images/pic04.jpg',
+			'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+		},
+		{
+			'title' : 'Blog post #5',
+			'thumbnail' : 'static/images/pic05.jpg',
+			'descr': 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.'
+		}
+	];
+
 }]);
